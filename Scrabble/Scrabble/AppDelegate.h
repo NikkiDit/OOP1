725236ee67a7764780bad301e4996a9bcf61a787
Adenike <adenikelatunji@yahoo.co.uk>
2014-10-31 18:47:02 +0000

    first commit

---
+++ Scrabble/Scrabble/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Scrabble
+//
+//  Created by Ryan Badham on 28/10/2014.
+//  Copyright (c) 2014 AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Scrabble/Scrabble/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Scrabble
+//
+//  Created by Ryan Badham on 28/10/2014.
+//  Copyright (c) 2014 AO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDataSource>
+
+@end
